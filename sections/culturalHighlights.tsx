9d7baab3953f1759627f22a2a6a5fc4e0c2c1065
KMV531 <koladjamomovinnybrayan3@gmail.com>
2026-01-20 13:18:55 +0100

    Cultural Higlights section completed, updated the dark-like shade on the hero section and fixed the globals.css color schemes

---
+++ sections/culturalHighlights.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import CardSwap, { Card } from "@/components/CardSwap";
+import Image from "next/image";
+
+const CulturalHighlightcards = [
+  {
+    image: "/images/japanese-food-image.jpg",
+    title: "Culinary Journeys",
+    description:
+      "Go beyond the sushi roll—discover the soul-warming depths of Ramen and the street-side sizzle of Takoyaki.",
+  },
+  {
+    image: "/images/japanese-sake-image.webp",
+    title: "The Sake Tradition",
+    description:
+      "Uncover the delicate craftsmanship behind Japan's national drink in historical brewing districts.",
+  },
+  {
+    image: "/images/japanese-street-food-image.jpg",
+    title: "Street Food Culture",
+    description:
+      "Experience the vibrant energy of night markets where sizzling grills offer a taste of local life.",
+  },
+];
+
+const CulturalHighlights = () => {
+  return (
+    <main className="max-w-5xl mx-auto flex items-center justify-between">
+      <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl max-w-md">
+        Essential Highlights: Culture & Landmarks
+      </h1>
+      <div style={{ height: "450px", position: "relative" }}>
+        <CardSwap
+          cardDistance={60}
+          verticalDistance={70}
+          delay={5000}
+          pauseOnHover={false}
+        >
+          {CulturalHighlightcards.map((CulturalHighlightcard) => (
+            <Card className="p-4" key={CulturalHighlightcard.title}>
+              <h3 className="font-bold">{CulturalHighlightcard.title}</h3>
+              <p className="font-semibold">
+                {CulturalHighlightcard.description}
+              </p>
+              <Image
+                src={CulturalHighlightcard.image}
+                alt={CulturalHighlightcard.title}
+                width={450}
+                height={100}
+                className="object-cover rounded-md mt-2"
+              />
+            </Card>
+          ))}
+        </CardSwap>
+      </div>
+    </main>
+  );
+};
+
+export default CulturalHighlights;
