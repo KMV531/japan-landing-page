8f02363417248e6e6ae0316609b72d9ab61837d3
KMV531 <koladjamomovinnybrayan3@gmail.com>
2026-01-22 17:45:08 +0100

    Removed the translated text and Added SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,59 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
+// URL canonique - à adapter selon l'environnement
+const canonicalUrl = process.env.NEXT_PUBLIC_SITE_URL || "";
+
 export const metadata: Metadata = {
   title: "Japan: Immersion in the Land of the Rising Sun",
   description:
     "From the electric bustle of Shibuya to the secret alleyways of Kyoto, explore the many facets of Japan. Between myth and reality, discover what makes this culture so fascinating.",
+  creator: "Vinny Brayan",
+  publisher: "Vinny Brayan",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  metadataBase: new URL(canonicalUrl),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Japan: Immersion in the Land of the Rising Sun",
+    description:
+      "From the electric bustle of Shibuya to the secret alleyways of Kyoto, explore the many facets of Japan. Between myth and reality, discover what makes this culture so fascinating.",
+    url: canonicalUrl,
+    siteName: "JAPAN",
+    locale: "en_EN",
+    type: "website",
+    images: [
+      {
+        url: `${canonicalUrl}/images/Thumbnail.png`, // Créez une image optimisée pour les réseaux sociaux
+        width: 1200,
+        height: 630,
+        alt: "Japan: Immersion in the Land of the Rising Sun",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Japan: Immersion in the Land of the Rising Sun",
+    description:
+      "From the electric bustle of Shibuya to the secret alleyways of Kyoto, explore the many facets of Japan. Between myth and reality, discover what makes this culture so fascinating.",
+    images: [`${canonicalUrl}/images/Thumbnail.png`], // Image spécifique pour Twitter
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
